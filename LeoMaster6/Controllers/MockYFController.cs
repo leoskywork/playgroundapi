97cac283ea0852f0d50465fc943014feff440757
Leo <leoskywork@outlook.com>
2020-12-07 22:45:30 +0800

    mock-yf update

--- LeoMaster6/Controllers/MockYFController.cs
+++ LeoMaster6/Controllers/MockYFController.cs
@@ -44,40 +44,103 @@ namespace LeoMaster6.Controllers
 
         #region Mocking
 
-        [HttpGet] //should be POST
-        [Route("customer/login")]
-        public IHttpActionResult Get_()
-        {
-
-            var user = new
-            {
-                identity = 100,
-                name = "刘立想",
-                gender = "man",
-                tel = "18612341234",
-                iconUrl = "https://leoskywork.com/img/seagull480.jpg",
-                availableBalance = 200,
-                unavailableBalance = 0,
-                regTime = 1577808000000,
-                source = "testing",
-                balance = 300,
-                deleted = false
-            };
-
-            return Json(new { USER = user });
-        }
 
         [HttpGet]
         [Route("shared/randomCode")]
-        public object Get__()
+        public object Get_()
         {
-            return Newtonsoft.Json.JsonConvert.SerializeObject(new
-            {
-                code = 123456
-            });
+            //return Newtonsoft.Json.JsonConvert.SerializeObject(new
+            //{
+            //    code = 123456
+            //});
+            return WrapResultJson("123456");
         }
 
 
+        [HttpGet] //should be POST
+        [Route("customer/login")]
+        public IHttpActionResult Get__()
+        {
+            var user = MockUser.GetOne();
+            return WrapResultJson(new { USER = user });
+        }
+
+        [HttpGet]
+        [Route("account/getCurrentUser")]
+        public IHttpActionResult Get___()
+        {
+            var user = MockUser.GetOne();
+            return WrapResultJson(user);
+        }
+
+
+       
+
+        [HttpPost]
+        [Route("account/getBalance")]
+        public IHttpActionResult Get____()
+        {
+            return WrapResultJson(MockUser.GetBalance());
+        }
+
+        [HttpGet]
+        [Route("account/record")]
+        public IHttpActionResult Get_____()
+        {
+            return WrapResultJson("demo transaction list", false, null, true);
+        }
+
         #endregion
+
+        private class MockUser
+        {
+            internal readonly static double avaiBalance = 200;  //can withdraw to bank card
+            internal readonly static double unavaiBalance = 100; //can not withdraw to bank card
+
+
+            public static object GetOne()
+            {
+                return new
+                {
+                    identity = 100,
+                    name = "刘立想",
+                    gender = "man",
+                    tel = "18612341234",
+                    // iconUrl = "https://leoskywork.com/img/seagull480.jpg",
+                    iconUrl = "image/seagull480.jpg",
+                    availableBalance = avaiBalance,
+                    unavailableBalance = unavaiBalance,
+                    regTime = 1577808000000,
+                    source = "testing",
+                    balance = GetBalance(),
+                    deleted = false
+                };
+            }
+
+            public static double GetBalance()
+            {
+                return avaiBalance + unavaiBalance;
+            }
+        }
+
+
+        //class ApiResult<T>
+        public IHttpActionResult WrapResultJson<T>(T data, bool hasError = false, string message = null, bool simpleVersion = false)
+        {
+            return Json(WrapResult(data, hasError, message, simpleVersion));
+        }
+
+        public static object WrapResult<T>(T data, bool hasError = false, string message = null, bool simpleVersion = false)
+        {
+            if (simpleVersion) return new { data, message };
+
+            return new
+            {
+                data,
+                success = !hasError,
+                message,
+                map = data//bad design, just for compatible
+            };
+        }
     }
 }
